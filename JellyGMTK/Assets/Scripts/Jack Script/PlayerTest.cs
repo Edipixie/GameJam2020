b98f81da89ab3b92f5cd092c2915aca1a2298f8c
Jack Russell Brindle <jackrussellbrindle@outlook.com>
2020-07-11 21:13:41 +0100

    Props

--- JellyGMTK/Assets/Scripts/Jack Script/PlayerTest.cs
+++ JellyGMTK/Assets/Scripts/Jack Script/PlayerTest.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 
 public class PlayerTest : MonoBehaviour
 {
+    string PlayerDir = "north";
     // Start is called before the first frame update
     void Start()
     {
@@ -18,24 +19,34 @@ public class PlayerTest : MonoBehaviour
 
     public void PlayerMoveFunction()
     {
-        if (Input.GetKeyDown("w"))
+        if (PlayerDir == "north")
         {
-            this.transform.position += new Vector3(0f, 0f, 0.5f);
-        }
-        else if (Input.GetKeyDown("d"))
-        {
-            this.transform.position += new Vector3(0.5f, 0f, 0f);
-        }
-        else if (Input.GetKeyDown("a"))
-        {
-            this.transform.position += new Vector3(-0.5f, 0f, 0f);
-        }
-        else if (Input.GetKeyDown("s"))
-        {
-            this.transform.position += new Vector3(0f, 0f, -0.5f);
-        } else if (Input.GetKeyDown("q"))
-        {
-            //this.transform.rotation += new Vector3(0f, 0f, 0f);
+            if (Input.GetKeyDown("w"))
+            {
+                this.transform.position += new Vector3(0f, 0f, 0.5f);
+            }
+            else if (Input.GetKeyDown("d"))
+            {
+                this.transform.position += new Vector3(0.5f, 0f, 0f);
+            }
+            else if (Input.GetKeyDown("a"))
+            {
+                this.transform.position += new Vector3(-0.5f, 0f, 0f);
+            }
+            else if (Input.GetKeyDown("s"))
+            {
+                this.transform.position += new Vector3(0f, 0f, -0.5f);
+            }
+            else if (Input.GetKeyDown("q"))
+            {
+                this.transform.rotation = Quaternion.Euler(0f, -90f, 0f);
 
+            }
+            else if (Input.GetKeyDown("e"))
+            {
+                this.transform.rotation = Quaternion.Euler(0f, -90f, 0f);
+
+            }
         }
+    }
 }
